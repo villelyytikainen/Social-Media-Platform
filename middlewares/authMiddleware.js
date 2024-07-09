f572a6c9d615eed94615cc8dd9f395a45df9906f
villelyytikainen <corpsefin@gmail.com>
2024-07-09 19:09:12 +0300

    token middleware working completely now

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -9,7 +9,7 @@ const authenticateToken = (req, res, next) => {
         return res.status(401).json({ message: "No token provided" });
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    jwt.verify(token, process.env.SM_JWTTOKEN, (err, decoded) => {
         if (err) {
             return res.status(401).json({ message: "Invalid token" });
         }
