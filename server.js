58e19e4186aabb8ab4cb072a57232d38e7fc8565
unknown <corpsefin@gmail.com>
2025-01-09 16:08:56 +0200

    refactor: app port changes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const messageRoutes = require("./routes/messages");
 const authenticateToken = require("./middlewares/authMiddleware");
 const { errorHandling } = require("./middlewares/errorHandling");
 const { Server } = require("socket.io");
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "frontend/dist")));
@@ -34,6 +35,6 @@ app.get("*", (req, res) => {
 app.use(cors());
 app.use(errorHandling);
 
-app.listen(process.env.DEV_PORT, () => {
-    console.log(`Server started on port http://localhost:${process.env.DEV_PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port http://localhost:${process.env.PORT}`);
 });
